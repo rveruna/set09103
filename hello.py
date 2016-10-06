5b4d8448a315951150367aa62e970c9a742291d6
verunar <verunar@seznam.cz>
2016-10-06 14:56:13 +0000

    commit all up to mask image

--- hello.py
+++ hello.py
@@ -1,9 +1,16 @@
-from flask import Flask
+from flask import Flask, url_for
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
   return "Hello World!"
+
+@app.route('/test')
+def css():
+  start = '<img src="'
+  url = url_for('static', filename='vmask.jpg')
+  end = '">'
+  return start+url+end,200
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0')
