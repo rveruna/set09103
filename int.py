5870db0cdc3597919cfe232654048ebdf9f48cbb
verunar <verunar@seznam.cz>
2016-10-13 15:37:59 +0000

    chapter5

---
+++ int.py
@@ -0,0 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/add/<int:first>/<int:second>")
+def add(first, second):
+  return str(first+second)
+
+if __name__ == "__main__":
+  app.run(host='0.0.0.0', debug=True)
---
+++ png.py
@@ -0,0 +1,26 @@
+from flask import Flask, request, url_for
+app = Flask(__name__)
+
+@app.route("/display/")
+def display():
+  return '<img src="'+url_for('static', filename='uploads/vmask.jpg')+'"/>'
+@app.route("/upload/", methods=['POST','GET'])
+def account():
+  if request.method == 'POST':
+    f = request.files['datafile']
+    f.save('static/uploads/vmask.jpg')
+    return "File Uploaded"
+  else:
+    page='''
+    <html>
+    <body>
+    <form action="" method="post" name="form" enctype="multipart/form-data">
+    <input type="file" name="datafile" />
+    <input type="submit" name="submit" id="submit"/>
+    </form>
+    </body>
+    </html>
+    '''
+    return page, 200
+if __name__ == "__main__":
+  app.run(host='0.0.0.0', debug=True)
